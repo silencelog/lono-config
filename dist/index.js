0b57475022aac4640618bb6fa11c668833440f40
ycl <455166539@qq.com>
2020-07-29 14:47:59 +0800

    项目初始化

---
+++ dist/index.js
@@ -0,0 +1,120 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = _default;
+
+var _path = _interopRequireDefault(require("path"));
+
+var _http = _interopRequireDefault(require("http"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+const CONTEXT_CONFIG = Symbol('context#config');
+/**
+ * 默认参数
+ * [defaultConfig description]
+ * @type {Object}
+ */
+
+const defaultConfig = {
+  env: process.env.LODE_ENV || 'index',
+  cwd: process.cwd()
+}; // TODO 消息订阅 配置修改
+
+class Config {
+  constructor(opt) {
+    this.name = 'config';
+    this.isLode = true;
+    this.opts = opt ? [opt] : [];
+    this.config = {};
+  }
+
+  install(lode) {
+    if (lode.context.hasOwnProperty(CONTEXT_CONFIG)) return; // 核心注入
+
+    this.opts.forEach(opt => {
+      const env = opt.env || defaultConfig.env;
+      let config; // 读取配置文件
+
+      if (opt.path) {
+        config = require(`${opt.path}${env}`); // 读取配置对象
+      } else if (opt.config) {
+        config = opt.config; // 读取远程地址json文件
+      } else if (opt.url) {
+        this.loadJSON(opt.url); // 读取配置中心配置
+      } else if (opt.center) {// 设置一个等待消息，等eureka连接执行
+        // if (lode.$observer) {
+        //   lode.$observer.on(`$eureka:mounted`, () => {})
+        // }
+        // config = opt.config
+        // 读取数据库配置
+      } else {
+        try {
+          config = require(`${defaultConfig.cwd}/config/${env}`);
+        } catch (e) {
+          throw new Error('config file is not find');
+        }
+      }
+
+      this.config = !this.config || opt.needClean === true ? Object.assign({}, defaultConfig, config) : Object.assign(this.config, config);
+    });
+    Object.defineProperty(lode, '$config', {
+      value: this.config,
+      writable: false
+    });
+    Object.defineProperties(lode.context, {
+      [CONTEXT_CONFIG]: {
+        value: lode.$config,
+        writable: false
+      },
+      'config': {
+        value: lode.$config,
+        writable: false
+      }
+    });
+  }
+
+  use(opt = {}) {
+    opt && this.opts.push(opt);
+    return this;
+  }
+
+  setConfig(lode, config) {
+    lode.$observer.emit(`$config:update`, config);
+  }
+
+  loadJSON(url) {
+    return _asyncToGenerator(function* () {
+      const json = yield loadJSON(url);
+      console.log('loadJSON', json);
+    })();
+  }
+
+}
+
+function loadJSON(url) {
+  return new Promise(function (resolve, reject) {
+    _http.default.get(url, function (res) {
+      let html = '';
+      res.on('data', data => {
+        html += data;
+      });
+      res.on('end', () => {
+        resolve(html);
+      });
+      res.on('error', e => {
+        reject(e.message);
+      });
+    });
+  });
+}
+
+function _default(...arg) {
+  return new Config(...arg);
+}
